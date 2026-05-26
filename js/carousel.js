2189063e02c31b2cef923626630a6d1a24d9e8f6
Jonatas <jonatasargologouveia@gmail.com>
2026-05-26 10:08:25 -0300

    salvar essa bosta na branch certa

--- js/carousel.js
+++ js/carousel.js
@@ -1,25 +1,23 @@
-
-
 //carousel
 
 //Array storage class
 let carouselArr = [];
 
-
 //class Carousel
 class Carousel {
-    constructor(){
-        
+    constructor(image, text, path){
+        this._image = image;
+        this.text = text;
+        this._path = path;
     }
-    
-      
+
     static Start(arr){
         if(arr){
 
             if(arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                Carousel.Next(0); //start
+                Carousel.renderizar(); //start
                 Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
             }
             
@@ -28,5 +26,48 @@ class Carousel {
         }
     }
 
-    
+     static renderizar(){
+        let img = document.getElementById("carousel");
+        let title = document.getElementById("carousel-title");
+
+        let info = carouselArr[Carousel._sequence];
+        title.style.color = "#000000";
+        img.innerHTML = `
+        <button id=prev_b><</button>
+        <a href="${info._path}"><img src="img/${info._image}" alt=""></a>
+        <button id=next_b>></button>`;
+        title.innerHTML = `<h2>${info.text}</h2>`;
+        Carousel.butao();
+
+     }
+    static Next(){
+        Carousel._sequence++;
+        if(Carousel._sequence > Carousel._size - 1){
+            Carousel._sequence = 0;
+        }
+
+        Carousel.renderizar();
+    }
+
+    static Previous(){
+        Carousel._sequence--;
+        if(Carousel._sequence < 0){
+            Carousel._sequence = Carousel._size - 1;
+        }
+
+        Carousel.renderizar();
+    }
+
+    static butao(){
+        document.getElementById("prev_b").addEventListener("click", () => {
+            clearInterval(Carousel._interval);
+            this().Previous();
+            Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+        })
+        document.getElementById("next_b").addEventListener("click", () => {
+            clearInterval(Carousel._interval);
+            this().Next();
+            Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+        });
+    }
 };
