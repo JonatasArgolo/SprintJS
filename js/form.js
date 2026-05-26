aafc6120ff4468634e9e5a53fc0ba5f3bbbdc4cb
Jonatas <jonatasargologouveia@gmail.com>
2026-05-26 10:52:29 -0300

    Igor

--- js/form.js
+++ js/form.js
@@ -2,8 +2,9 @@
 //class contato
 
 class contato {
-    constructor(nome, email, telefone, contato, avaliacao) {
+    constructor(nome, sobrenome, email, telefone, contato, avaliacao) {
         this.nome = nome;
+        this.sobrenome = sobrenome;
         this.email = email;
         this.telefone = telefone;
         this.contato = contato;
@@ -14,6 +15,7 @@ class contato {
 function Post(form) {
 
     let data = new contato(form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value,
         form.elements.namedItem("email").value,
         form.elements.namedItem("telefone").value,
         form.elements.namedItem("contato").value,
@@ -30,9 +32,10 @@ function Enviar(consol, form) {
     document.addEventListener("submit", (e) => {
         e.preventDefault();
         var nome = document.getElementById("nomeid");
+        var sobrenome = document.getElementById("sobrenomeid");
 
         if (nome.value != "") {
-            alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+            alert('Obrigado sr(a) ' + nome.value + ' ' + sobrenome.value + ' os seus dados foram encaminhados com sucesso');
         }
         console.log(consol);
         form.reset();
